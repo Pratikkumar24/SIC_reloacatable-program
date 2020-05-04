53019470887f244fbf1ba2e31c8abf934639e466
Pratik24 <pratikkumar200024@gmail.com>
2020-05-04 17:02:34 +0530

    modified main file

--- sic_main.cpp
+++ sic_main.cpp
@@ -1,133 +1,11 @@
-#include "opcode.h"
+#include "symtab.h"
 #include <fstream>
 #include <sstream>
 using namespace std;
-int loctr = 0;
 
-map<string, long> mpopcode;
-map<string, long>::iterator it;
-int conversion(int n)
-{
-    int temp, hex = 0, temp1;
-    int i = 0;
-    while (n != 0)
-    {
-        temp = n % 10;
-        hex += (pow(16, i) * temp);
-        n /= 10;
-        i++;
-    }
-    return hex;
-}
-int ispresent(string word)
-{
-
-    it = mpopcode.find(word);
-    if (it == mpopcode.end())
-    {
-        return 1;
-    }
-    else
-        return 0;
-}
-string get_object_code(string ch)
-{
-    int k;
-    string word;
-    stringstream iss(ch);
-
-    while (iss >> word)
-    {
-        if (word == "END")
-        {
-            cout << "\n ENd of the program";
-            return "OKAY";
-        }
-        else if (word == "START")
-        {
-            iss >> word;
-            stringstream stringtoint(word);
-
-            stringtoint >> loctr;
-            loctr = conversion(loctr); //converting into decimal..so that addition becomes easy
-            cout << "\n Location pointer:" << loctr;
-        }
-        else
-        {
-            if (isoptab(word) == 0) //symbol
-            {
-
-                if (ispresent(word) && loctr != 0) //is symbol already present in the symtab?
-                {                                  //if NO
-                    if (word == "WORD")
-                    {
-                        loctr += 3;
-                        iss >> word;
-                    }
-                    else if (word == "RESW")
-                    {
-                        iss >> word;
-
-                        stringstream stringtoint(word);
-                        stringtoint >> k;
-                        k *= 3;
-                        loctr += k;
-                    }
-                    else if (word == "RESB")
-                    {
-                        iss >> word;
-                        stringstream stringtoint(word);
-                        stringtoint >> k;
-                        loctr += k;
-                    }
-                    else if (word == "BYTE") // C'EOF'..find the length of byte..hereit is 3
-                    {
-                        int counter = 0;
-                        iss >> word;
-                        for (int i = 0; word[i]; i++)
-                        {
-                            if (word[i] == '\'')
-                            {
-                                int k = i + 1;
-
-                                while (!(word[k] == '\''))
-                                {
-                                    counter++;
-                                    k++;
-                                }
-                                break;
-                            }
-                        }
-                        loctr += counter;
-                    }
-                    else
-                    {
-                        mpopcode.insert({word, loctr});
-                    }
-                }
-                else if (loctr != 0)
-                { //if no
-                    cout << "\n ITS an ERROR..ITS ALREADy PRESENT IN THE SYMTAB";
-                    cout << "\n\n\t\t TERMINATING";
-                    exit(0);
-                }
-            }
-            else if (isoptab(word)) //opcode
-            {
-                loctr += 3;
-                iss >> word;
-            }
-        }
-    }
-
-    cout << endl;
-
-    return ch;
-}
 int main()
 {
-    // int i = 1000;
-    // cout << "\n After converting:" << hex << i << endl;
+
     ifstream fin("lineofcodes.txt");
     ofstream fout("objectcode.txt");
 
@@ -147,5 +25,6 @@ int main()
     {
         cout << it->first << " => " << it->second << endl;
     }
+    cout << "\n Length of the program:" << loctr;
     return 0;
 }
