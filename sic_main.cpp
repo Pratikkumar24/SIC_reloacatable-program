f44500556f008da9eece3edda6797d40faa40f9a
Pratik24 <pratikkumar200024@gmail.com>
2020-05-04 12:06:28 +0530

    setup of location counter

--- sic_main.cpp
+++ sic_main.cpp
@@ -1,16 +1,48 @@
 #include "opcode.h"
+#include <fstream>
+#include <sstream>
 using namespace std;
+long int loctr = 0;
+// map<string, int> mpopcode;
+string get_object_code(string ch)
+{
+    ifstream fin("lineofcodes.txt");
+    string word;
+    while (fin >> word)
+    {
+        if (word == "START")
+        {
+            fin >> word;
+            stringstream stringtoint(word);
+
+            stringtoint >> loctr;
+            cout << "\n Location pointer:" << loctr;
+        }
+        else
+        {
+
+            cout << "\n this the word:" << word;
+            cout << "\n opcode of this word is:" << optab(word) << endl;
+        }
+    }
+    return ch;
+}
 int main()
 {
-    string p = "s";
-    p = optab(p);
-    if (p == "ERROR")
+
+    ifstream fin("lineofcodes.txt");
+    ofstream fout("objectcode.txt");
+
+    string lineofcode, value;
+    char c;
+    int count = 0;
+    while (!fin.eof())
     {
-        cout << "\n Its an error";
+        getline(fin, lineofcode);
+        string value = get_object_code(lineofcode);
+        fout << value << endl;
+        count++;
     }
-    else
-    {
-        cout << "\n Opcode:" << p;
-    }
+    cout << "\n\n Number of lines:" << count;
     return 0;
 }
